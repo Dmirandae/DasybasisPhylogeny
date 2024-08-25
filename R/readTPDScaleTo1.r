e6e285944d0ee64b1c49052c922085ce4c826bb3
dmirandae <dmirandae@gmail.com>
2024-08-24 21:50:06 -0500

    cluster?

--- R/readTPDScaleTo1.r
+++ R/readTPDScaleTo1.r
@@ -1,10 +1,13 @@
 library(geomorph)
 
+packageVersion("geomorph")
+
 ## get a rps file and write it scaled with id
 
 getwd()
 
 rutaDatos <- "/home/rafael/proyectosDRME/tabanos/Dasybasis/2024/landmarks/2024/Alas-Dasybasis"
+
 
 archivosTps <- list.files(path=rutaDatos,"tps")
 
@@ -14,27 +17,91 @@ archivosTps
 
 #output_file <- paste(rutaDatos,"mod",archivosTps[1],sep="")
 
-DasybasisLM <- readmulti.tps(archivosTps,readcurves = FALSE,specID = "ID")
+DasybasisLM <- readmulti.tps(
+  paste0(rutaDatos,"/",archivosTps,sep=""),
+  readcurves = FALSE,specID = "ID")
 # b <- readland.tps(input_file,readcurves = FALSE,specID = "ID")
 
-DasybasisLM
+shortDasybasis <- DasybasisLM[-c(29,30),,]
+
+shortDasybasis[29,,]
 
 getTheID <- function(TPSobject) {attr(TPSobject, "dimnames")[[3]]}
 
-sapply(DasybasisLM,getTheID)
 
 getTheID(DasybasisLM)
+getTheID(shortDasybasis)
+
+
 
 
 
 ####
-writeland.tps(b, output_file,specID = TRUE)
+##writeland.tps(DasybasisLM,"todoDasybasis.nts",specID = TRUE)
+
+#?gpagen
+
+## quizas no asi, dado que la falta dle lm pouede ser una caracteristica
 
 
-str(b)
-class(b)
+#?estimate.missing
+
+DasybasisTPS <- estimate.missing(DasybasisLM,method = "TPS")
+DasybasisREG <- estimate.missing(DasybasisLM,method = "Reg")
+
+### TPS
+
+CamaUnica <- gpagen(DasybasisTPS,Parallel = T)
+
+shortCamaUnica  <- gpagen(DasybasisTPS,Parallel = T)
+
+##?gm.prcomp
+
+DasybasyisPCA <- gm.prcomp(CamaUnica$coords)
+
+shortDasybasyisPCA <- gm.prcomp(shortCamaUnica$coords)
+
+summary(DasybasyisPCA)
+summary(shortDasybasyisPCA)
+
+
+par(mfrow = c(1, 2))
+
+plot(DasybasyisPCA)
+plot(shortDasybasyisPCA)
+
+
+### Reg
+
+CamaUnica <- gpagen(DasybasisREG,Parallel = T)
+
+##?gm.prcomp
+
+DasybasyisPCA <- gm.prcomp(CamaUnica$coords)
+
+summary(DasybasyisPCA)
+
+plot(DasybasyisPCA)
 
 
 
 
 
+
+dist(DasybasyisPCA$A)
+
+
+
+##### coords??? 
+                                        # Extract Procrustes coordinates
+coords <- gpa$coords
+
+                                        # Choose a distance metric
+dist_matrix <- dist(coords, method = "euclidean")  # Replace "euclidean" with your preferred metric
+
+                                        # Perform K-means clustering
+k <- 3  # Choose the number of clusters
+kmeans_result <- kmeans(dist_matrix, centers = k)
+
+                                        # Visualize results (e.g., using plot.kmeans)
+plot(kmeans_result)
