a1f2e74bc89d0158581045d29347f3d6c795cbe1
dmirandae <dmirandae@gmail.com>
2024-08-25 22:23:37 -0500

    piwe 3

--- chars/busqueda01.sh
+++ chars/busqueda01.sh
@@ -1,1 +1,1 @@
-tnt  mxram 500 \; p DasybasisMorphology2015.mtr \; hold 100000 \; xmult = rep 25000 \;
+tnt  mxram 500 \; p DasybasisMorphology2015.mtr \; hold 100000 \; piwe = 3 \;xmult = rep 25000 \;
