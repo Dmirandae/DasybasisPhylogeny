95c7381be74612fc2e47ab0a00be7ce57ae9722f
dmirandae <dmirandae@gmail.com>
2024-08-27 00:22:54 -0500

    standard morphology

---
+++ chars/busqueda00.sh
@@ -0,0 +1,1 @@
+tnt  mxram 500 \; p DasybasisMorphology2015.mtr \; hold 10000 \; piwe = 3 \;xmult = rep 2500 \;
--- chars/busqueda01.sh
+++ chars/busqueda01.sh
@@ -1,1 +1,1 @@
-tnt  mxram 500 \; p DasybasisMorphology2015.mtr \; hold 100000 \; piwe = 3 \;xmult = rep 25000 \;
+tnt  mxram 500 \; p DasybasisMorphology2015.mtr \; log verpiwe3.out \;   hold 100000 \; piwe = 3 \;xmult = rep 25000 \; taxn = \; nel* \; export -DasybasisPiwe3.tre \; resample rep 10000 \; zz 
