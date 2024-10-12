cbae593e38d09430b01ccd05ceb95eed6ad0a087
dmirandae <dmirandae@gmail.com>
2024-10-12 10:20:34 -0500

    ev total final round

--- totalEvidence/revisarArboles.r
+++ totalEvidence/revisarArboles.r
@@ -1,11 +1,17 @@
 library(ape)
 library(phytools)
 #~ read.nexus("todosLosArboles.nex")
-arbolesDasybasisEvTotal <-  read.nexus("todosLosArboles.nex")
-arbolesDasybasisLM     <-  read.nexus("piwe2LM-compacto-ContraTabanus.nex")
-ape::dist.topo(unroot(arbolesDasybasisEvTotal))
-ape::dist.topo(unroot(arbolesDasybasisEvTotal),unroot(arbolesDasybasisLM))
-ape::dist.topo(unroot(arbolesRecortados),unroot(arbolesDasybasisLM))
+
+arbolesDasybasisEvTotal <-  read.nexus("tmp1.tre")
+#arbolesDasybasisLM     <-  read.nexus("piwe2LM-compacto-ContraTabanus.nex")
+# arbolesDasybasisEvTotal[[1]]
+
+plot(arbolesDasybasisEvTotal)
+
+ape::dist.topo(unroot(arbolesDasybasisEvTotal),unroot(arbolesDasybasisEvTotal))
+
+                                        #ape::dist.topo(unroot(arbolesDasybasisEvTotal),unroot(arbolesDasybasisLM))
+#ape::dist.topo(unroot(arbolesRecortados),unroot(arbolesDasybasisLM))
 
 
 #~ plot(arbolesDasybasisEvTotal)
