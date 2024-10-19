631386d0cb75f6680a2ea8512f0a8f3d4f7b7bd3
dmirandae <dmirandae@gmail.com>
2024-10-19 09:17:12 -0500

    last mile

---
+++ msData/finalEvaluarArboles.r
@@ -0,0 +1,61 @@
+library(ape)
+library(phytools)
+#~ read.nexus("todosLosArboles.nex")
+
+pdf("Phylogenies_Dasybasis_2024-10-19.pdf", paper="special",
+    width = 15,
+    height = 30)
+
+
+options(width = 500)
+                                        #
+#~ arbolesDasybasisEvTotal <-  read.nexus("DasybasisAllK26.tre")
+arbolesDasybasisEvTotal <-  read.nexus("finalTrees-Das-2024-10-18.tre")
+
+
+names(arbolesDasybasisEvTotal)
+
+for (arbol1 in 1:2){
+
+    for (arbol2 in arbol1:3){
+
+
+par(mfrow = c(1, 2)) 
+plot.phylo(arbolesDasybasisEvTotal[[arbol1]])
+plot.phylo(arbolesDasybasisEvTotal[[arbol2]])
+
+obj<-cophylo(arbolesDasybasisEvTotal[[arbol1]],arbolesDasybasisEvTotal[[arbol2]])
+
+par(mfrow = c(1, 1)) 
+
+plot(obj,type=c("cladogram","phylogram"),
+     fsize=0.9,part=0.2,pts=FALSE,
+     lwd=2,link.type="curved")
+
+
+    }
+}
+
+
+
+
+
+##dev.off()
+
+ape::dist.topo(unroot(arbolesDasybasisEvTotal))
+
+for (topologia in 1:3){
+
+plot(arbolesDasybasisEvTotal[[topologia]], main=names(arbolesDasybasisEvTotal)[topologia],
+     font = 2, cex = 2.3, label.offset = 1,
+     no.margin = TRUE)
+
+}
+
+plot(root(consensus(arbolesDasybasisEvTotal[1:3]),"Tabanus"),main="Consensus all",
+     font = 2.5, cex = 2.3, label.offset = 1,
+     no.margin = TRUE)
+
+plot(root(consensus(arbolesDasybasisEvTotal[1:2]),"Tabanus"),main="Consensus LM",
+     font = 3, cex = 2.3, label.offset = 1,
+     no.margin = TRUE)
