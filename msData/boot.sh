4521fb0585cf4630b8d1e178c88976db5ac8090c
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2024-10-22 08:25:58 -0500

    boot ev total

--- msData/boot.sh
+++ msData/boot.sh
@@ -1,2 +1,2 @@
-tnt p totalEvidenceDasybasis-2024-10-19.mtr \; log +bootEvtotalpiwe26.out  \; piwe = 26 \; ttag= \; taxna= \;  tsave \!15 bootTreeEvTotal$1 \; hold 500 \; resampl boo rep 100 [ mult= hold 1 replic 1 ] \; export -bootEvTotalPiwe26.tre \; 
+tnt p totalEvidenceDasybasis-2024-10-19.mtr \; rseed 0 \; log +bootEvtotalpiwe26.out  \; piwe = 26 \; ttag= \; taxn= \;  tsave \!9 bootTreeEvTotal$1 \; hold 200 \; resampl boo rep 100 [ mult= hold 1 replic 1 ] \; export -bootEvTotalPiwe26.tre \; 
 
