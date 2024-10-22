fccfa966367f6e81f5cadc9d3be5a53849c7a9e7
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2024-10-22 08:51:15 -0500

    boot not working

--- msData/manipularArbolesTmp.sh
+++ msData/manipularArbolesTmp.sh
@@ -1,1 +1,4 @@
-echo "tread 'Arboles'" > x0 | cat $1* | grep -v tread | tr -d ";" | tr -d "*" | grep -v trees | sort | uniq | sed 's/)$/)*/g' >> x0
+echo "tread 'Arboles'" > arbolesBootEvTotal   | cat tmp | grep -v  "()" | sed 's/);/)*/g' | grep -v tre  | grep -v partial|  sort | uniq  >> arbolesBootEvTotal 
+
+tnt p totalEvidenceDasybasis-2024-10-19.mtr \; piwe=26 \; p arbolesBootEvTotal \; taxn= \; export= bootEvtotalPiwe26.tree \; zz 
+
