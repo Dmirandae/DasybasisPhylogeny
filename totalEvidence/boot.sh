4a6a710a552202044387fb38b9057d076e1abb49
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2024-10-21 20:39:19 -0500

    boot ev total

---
+++ totalEvidence/boot.sh
@@ -0,0 +1,2 @@
+tnt p totalEvidenceDasybasis-2024-10-10.mtr \; log +bootEvtotalpiwe26.out  \; piwe = 26 \; ttag= \; taxna= \; resampl boo rep 1000 [ mult= hold 1 replic 1 ] \; export -bootEvTotalPiwe26.tre \; 
+
